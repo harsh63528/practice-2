ba633da90921c4a96fb32bd5fa6632ce466f78f6
harsh singh <vduharsh123@gmail.com>
2025-11-05 08:21:59 +0530

    use req.header.host for taking data from url

--- server.js
+++ server.js
@@ -1,39 +1,56 @@
-import http from 'node:http'
-import{countries,continents} from './data.js'
+    import http from 'node:http'
+    import{countries,continents} from './data.js'
 
-const server=http.createServer((req,res)=>{
-    const urlObj=new URL(req.url,`http://${req.headers.host}`)
-    console.log(urlObj)
-     if(req.url==='/' && req.method==='GET'){
-        res.writeHead(200)
-        res.end(JSON.stringify(continents));
-     }
+    const server=http.createServer((req,res)=>{
+        const urlObj=new URL(req.url,`http://${req.headers.host}`)
 
-    else if(req.url.startsWith('/')&& req.method==='GET'){
-      
-        res.writeHead(200,{'content-Type':'text/json'})
-        let data=req.url.split('/').pop();
-        const store=[];
-        const returnData=()=>{countries.filter(element=>{
-            if(element.continentCode.toLocaleLowerCase()===data.toLocaleLowerCase()){
-                store.push(element)
-            }
-        })
-        return store
-    }
-        res.end(JSON.stringify(returnData()),'utf-8')
-    }
+        // accessing pathname and paramenters
+        
+        const path=urlObj.pathname;
+        const  splitPath=path.split('/').filter(Boolean)
     
 
+        // set header type
+        res.setHeader('Content-Type','application/json;charset=utf-8')
 
-     else{
-        res.writeHead(404)
-        res.end(`${res.statusCode}:url not found`)
-     }
-})
+        // routing
+        if(req.url.startsWith('/') && splitPath.length !==0 && req.method==='GET'){
+            const data=splitPath[0];
 
-const PORT=8000;
+            // filtering country by code
+            async function findCountryByCode(data){
+                
+                const co=()=>{
+                    const co=[]
+                    countries.filter((country)=>{
+                if(country.code===data.toUpperCase()){
+                    co.push(country)
+                    res.end(JSON.stringify(country))
+                }
+            }
+        )
+        return co;
+    }  
+               return co();
+               
+            }
+            let give=findCountryByCode(data);
+             if(give.length===0){
+                    res.statusCode=404;
+                    res.end(JSON.stringify({error:res.statusCode,reason:'Not Found'}))
+                } 
+                
+                else{
+                    res.end(JSON.stringify(give),'utf-8')
+                }
+        
+            
+        }
+       
+    })
 
-server.listen(PORT,()=>{
-    console.log(`server:http://localhost:${PORT}`)
-})
+    const PORT=8000;
+
+    server.listen(PORT,()=>{
+        console.log(`server:http://localhost:${PORT}`)
+    })
