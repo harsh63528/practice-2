3806e1fd5f8467155de02ba558f959bda4f85d0e
harsh singh <vduharsh123@gmail.com>
2025-11-06 21:05:47 +0530

    all error solved

--- subdata.js
+++ subdata.js
@@ -1,18 +1,27 @@
+export default function subdata(data, errcode, urlObj, res) {
+  const subRegion = urlObj.searchParams.get('region') || '';
+  console.log(subRegion);
 
-
-    export default function subdata(data,errcode,urlObj,res){
-        const subRegion=urlObj.searchParams.get(`region`)||'';
-                    console.log(subRegion)
-                    if(data.statusCode===200){
-                        if(subRegion){
-                            res.statusCode=200
-                            return { response: res.statusCode, 
-                                subregion:  subRegion}
-                        }
-                    }
-                    else{
-
-                    res.statusCode=errcode;
-    return { statusCode: res.statusCode }
-                    }
-                } 
+  if (data.statusCode === 200) {
+    if (subRegion) {
+      res.statusCode = 200;
+      return {
+        statusCode: res.statusCode,
+        subregion: subRegion,
+      };
+    } else {
+      // Subregion is missing but status code is OK
+      res.statusCode = 200;
+      return {
+        statusCode: res.statusCode,
+        message: "Subregion not provided",
+      };
+    }
+  } else {
+    // Error case
+    res.statusCode = errcode;
+    return {
+      statusCode: res.statusCode,
+    };
+  }
+}
